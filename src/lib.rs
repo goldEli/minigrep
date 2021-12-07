fe10eddccbab4710fed9bf67a4e986cba654d650
miaoyu <miaoyu2009@qq.com>
2021-12-07 14:02:48 +0800

    🎉

---
+++ src/lib.rs
@@ -0,0 +1,91 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+	let contents = fs::read_to_string(config.filename.clone())?;
+	let lines = if config.case_sensitive {
+		search(&config.query[..], &contents[..])
+	} else {
+		search_insensitive(&config.query[..], &contents[..])
+	};
+	println!("with text:\n{}", contents);
+	for line in lines {
+		println!("\nmatch: \n{}", line);
+	}
+
+	Ok(())
+}
+
+pub struct Config {
+	pub query: String,
+	pub filename: String,
+	pub case_sensitive: bool,
+}
+
+impl Config {
+	pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+		args.next();
+		let query = match args.next() {
+			Some(query) => query,
+			None => return Err("Didn't get query string"),
+		};
+		let filename = match args.next() {
+			Some(filename) => filename,
+			None => return Err("Didn't get filename string"),
+		};
+
+		let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+		Ok(Config {
+			query,
+			filename,
+			case_sensitive,
+		})
+	}
+}
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+	content.lines()
+		.filter(|line| line.contains(query))
+		.collect()
+}
+
+fn search_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+	let query = query.to_lowercase();
+	let mut ret = vec![];
+	for line in content.lines() {
+		if line.to_lowercase().contains(&query) {
+			ret.push(line)
+		}
+	}
+	ret
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn case_sensitive() {
+		let query = "duct";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+			";
+
+		assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+	}
+
+	#[test]
+	fn case_insensitive() {
+		let query = "rUsT";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+			";
+
+		assert_eq!(vec!["Rust:"], search_insensitive(query, contents))
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use minigrep::Config;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        println!("Problem parse arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    // if let Err(e) = run(&config) {
+    //     println!("Application error: {}", e);
+    //     process::exit(1);
+    // };
+    minigrep::run(&config).unwrap_or_else(|err| {
+        println!("Application error: {}", err);
+        process::exit(1);
+    })
 }
