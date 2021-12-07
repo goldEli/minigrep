15d625391ea5459f5b509a1154ebfbc8e04ec4e3
miaoyu <miaoyu2009@qq.com>
2021-12-07 14:32:55 +0800

    ✨ pre-release preparation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,54 @@
+//! # Art
+//!
+//! A library for modeling artistic concepts.
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
+
+pub mod kinds {
+	/// The primary colors according to the RYB color model.
+	pub enum PrimaryColor {
+			Red,
+			Yellow,
+			Blue,
+	}
+
+	/// The secondary colors according to the RYB color model.
+	pub enum SecondaryColor {
+			Orange,
+			Green,
+			Purple,
+	}
+}
+
+pub mod utils {
+	use crate::kinds::*;
+
+	/// Combines two primary colors in equal amounts to create
+	/// a secondary color.
+	pub fn mix(c1: PrimaryColor, c2: PrimaryColor) {
+			// --snip--
+	}
+}
+
 use std::env;
 use std::error::Error;
 use std::fs;
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = minigrep::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+	x + 1
+}
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
 	let contents = fs::read_to_string(config.filename.clone())?;
